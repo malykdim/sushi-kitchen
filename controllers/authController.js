b857bf3f759727c3684880853f0f7dbed3c1b908
Albena Kostova <albena.kostov@gmail.com>
2022-11-03 20:34:08 +0100

    Login, Logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const authController = require('express').Router();
-const { register } = require('../services/userService');
+const { register, logout } = require('../services/userService');
 
 
 authController.post('/register', async (req, res) => {
@@ -11,6 +11,23 @@ authController.post('/register', async (req, res) => {
             message: error.message
         });
     }
+});
+
+authController.post('/login', async (req, res) => {
+    try {
+        const token = await login(req.body.email, req.body.password);
+        res.json(token);
+    } catch (error) {
+        res.status(401).json({
+            message: error.message
+        });
+    }
+});
+
+authController.get('/logout', async (req, res) => {
+    const token = req.headers['x-authorization'];
+    await logout(token);
+    res.status(204).end();
 });
 
 module.exports = authController;
--- services/userService.js
+++ services/userService.js
@@ -2,6 +2,8 @@ const User = require("../models/User");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+const tokenBlacklist = new Set();
+
 async function register(email, password) {
     const existing = await User.findOne({ email }).collation({ locale: 'en', strength: 2 });
     if (existing) {
@@ -13,19 +15,25 @@ async function register(email, password) {
         hashedPassword: await bcrypt.hash(password, 10)
     });
     
-    return {
-        _id: user._id,
-        email: user.email,
-        accessToken: createToken(user)
-    };
+    return createToken(user);
 }
 
 async function login(email, password) {
+    const user = await User.findOne({ email }).collation({ locale: 'en', strength: 2 });
+    if (!user) {
+        throw new Error('Incorrect email or password');
+    }
 
+    const match = await bcrypt.compare(password, user.hashedPassword);
+    if (!match) {
+        throw new Error('Incorrect email or password');        
+    }
+    
+    return createToken(user);
 }
 
-async function logout() {
-
+async function logout(token) {
+    tokenBlacklist.add(token);
 }
 
 function createToken(user) {
@@ -34,11 +42,15 @@ function createToken(user) {
         email: user.email
     };
     
-    return jwt.sign(payload, process.env.TOKEN_SECRET);
+    return {
+        _id: user._id,
+        email: user.email,
+        accessToken: jwt.sign(payload, process.env.TOKEN_SECRET)
+    };
 }
 
 function parseToken(token) {
-    
+    // TODO: scan blacklist for token
 }
 
 module.exports = {
