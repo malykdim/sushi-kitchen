b7512631967ee40e1260c8d9e9c1c72c531df93a
Albena Kostova <albena.kostov@gmail.com>
2022-11-05 16:03:54 +0100

    comment - isAdmin

--- models/User.js
+++ models/User.js
@@ -1,17 +1,18 @@
 const { Schema, model } = require('mongoose');
 
-const userSchema = new Schema({    
+const userSchema = new Schema({
     email: { type: String, required: true, unique: true },
     hashedPassword: { type: String, required: true },
+    // isAdmin: { type: Boolean, default: false, required: true },
     role: {
-        type: String, 
+        type: String,
         enum: {
-            values: ['client', 'chef', 'admin'], 
+            values: ['client', 'chef', 'admin'],
             message: props => `${Object.keys(props)}({VALUE}) is not supported`
         },
-        default: 'client' 
+        default: 'client'
     }
-});
+}, { timestamps: true });
 
 userSchema.index({ email: 1 }, {
     collation: {
